344d2736182f75f00b8d7e5bf4b3fbc8297986ed
VSkarm <vskarm93@gmail.com>
2023-03-05 20:56:50 +0200

    Changes to Css

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 const button = document.querySelector(".navbar__ham");
 const menu = document.querySelector(".navbar__links");
+const overlay = document.querySelector("#overlay");
 
-
-button.addEventListener("click", function() {
+button.addEventListener("click", function(){
   button.classList.toggle("open");
   menu.classList.toggle("navbar__open");
-
+  overlay.classList.toggle("show");
 });
+
+overlay.addEventListener("click", function(){
+  overlay.classList.toggle("show");
+  button.classList.toggle("open");
+  menu.classList.toggle("navbar__open");
+});
+
+
+function updateTime() {
+  const now = new Date();
+  const year = now.getFullYear().toString().slice(-2);
+  const month = ('0' + (now.getMonth() + 1)).slice(-2);
+  const day = ('0' + now.getDate()).slice(-2);
+  const hours = ('0' + now.getHours()).slice(-2);
+  const minutes = ('0' + now.getMinutes()).slice(-2);
+  const datetimeStr = `${day}/${month}/${year} ${hours}:${minutes}`;
+  document.getElementById('datetime').innerHTML = datetimeStr;
+}
+
+updateTime();
+setInterval(updateTime, 60000); // Update every minute
+
